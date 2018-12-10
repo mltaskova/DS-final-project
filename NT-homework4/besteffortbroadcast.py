4981d0a8ca51bd44f5d68f1e9f50fb7797849326
Natalie Truong <nguyentruongfs@gmail.com>
2018-12-09 22:38:45 -0500

    Chat application with a server to keep track of friend list

--- NT-homework4/besteffortbroadcast.py
+++ NT-homework4/besteffortbroadcast.py
@@ -3,19 +3,19 @@ from perfectpointtopointlinks import PerfectPointToPointLinks
 
 class BestEffortBroadcast:
 
-    def __init__(self, process_id, process_id_list, addr_str, arg_callback):
+    def __init__(self, process_id, addr_str, callback, arg_callback):
         self.address = addr_str
-        self.deliver_call_back = arg_callback
-        self.links = PerfectPointToPointLinks(port=process_id, addr_str=addr_str, arg_callback = self.deliver)
-        self.process_id_list = process_id_list
-
-    def broadcast(self, message):
-        for process_id in self.process_id_list:
-            self.links.send(process_id, self.address, message)
+        self.deliver_call_back = callback
+        self.arg_callback = arg_callback
+        self.links = PerfectPointToPointLinks(port=process_id, addr_str=addr_str, arg_callback=self.deliver)
 
     def close(self):
         self.links.close()
 
     def deliver(self, sender_id, message):
         if message is not None:
-            self.deliver_call_back((self.address+str(sender_id), message))
+            self.deliver_call_back((sender_id, message), self.arg_callback)
+
+    def broadcast(self, message, process_id_list):
+        for process_id in process_id_list:
+            self.links.send(process_id, self.address, message)
--- NT-homework4/perfectpointtopointlinks.py
+++ NT-homework4/perfectpointtopointlinks.py
@@ -1,41 +1,41 @@
 import socket
-import threading
+from multiprocessing import Process
+
 
 class PerfectPointToPointLinks:
     def __init__(self, port, addr_str, arg_callback):
-        self.deliver_callback = arg_callback
         self.port = port
         self.address = addr_str
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.bind((self.address, int(self.port)))
         self.server_socket.listen(4)
         self.deliver_list = []
-        threading.Thread(target=self.deliver, args=()).start()
-
+        self.p = Process(target=self.deliver, args=(arg_callback,))
+        self.p.start()
 
     def send(self, recipient_process_port, addr_str, message):
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
-            client_socket.connect((addr_str, recipient_process_port))
+            client_socket.connect((addr_str, int(recipient_process_port)))
         except ConnectionRefusedError:
             print("Friend {} has not joined.".format(recipient_process_port))
-            return
         mesg = str(self.port) + "+" + message
         client_socket.send(mesg.encode())
         client_socket.close()
 
-    def deliver(self):
+    def deliver(self, arg_callback):
         while True:
             (connection, address) = self.server_socket.accept()
             buf = connection.recv(2048)
             message = None
             if buf:
                 message = buf.decode()
-                recipient_port = message.split("+")[0]
+                sender_port = message.split("+")[0]
                 message = message.split("+")[1]
             connection.close()
-            self.deliver_callback(recipient_port, message)
-
+            arg_callback(int(sender_port), message)
 
     def close(self):
         self.server_socket.close()
+        self.p.terminate()
